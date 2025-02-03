941f2550c71f11fff886169ba1dd0ec67c809d6e
Alan Garber <a8garber@Alans-MacBook-Air.local>
2025-02-03 14:10:12 -0500

    rel = noreferrer

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,11 +7,11 @@ function Navbar() {
     <div className="navbar">
     <div>Alan</div>
     <div className="navbar-links">
-        <div><a href="https://alanmgarber.com" target="_blank">PORTFOLIO</a></div>
-        <div><a href="https://www.linkedin.com/in/alan-garber-659063112/" target="_blank">LINKEDIN</a></div>
-        <div><a href="https://github.com/OldEphraim" target="_blank">GITHUB</a></div>
-        <div><a href="https://oldephraimlearnstocode.com" target="_blank">BLOG</a></div>
-        <div><a href="https://alanmgarber.com/Resume" target="_blank">RESUME</a></div>
+        <div><a href="https://alanmgarber.com" target="_blank" rel="noreferrer">PORTFOLIO</a></div>
+        <div><a href="https://www.linkedin.com/in/alan-garber-659063112/" target="_blank" rel="noreferrer">LINKEDIN</a></div>
+        <div><a href="https://github.com/OldEphraim" target="_blank" rel="noreferrer">GITHUB</a></div>
+        <div><a href="https://oldephraimlearnstocode.com" target="_blank" rel="noreferrer">BLOG</a></div>
+        <div><a href="https://alanmgarber.com/Resume" target="_blank" rel="noreferrer">RESUME</a></div>
         <div><a href="mailto:a8garber@yahoo.com">CONTACT</a></div>
         </div>
         </div>
